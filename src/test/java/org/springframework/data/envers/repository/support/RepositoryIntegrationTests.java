4262bb777c9a9d4f562163c95f3b4ce35985b4dc
Oliver Gierke <info@olivergierke.de>
2015-12-20 20:04:42 +0100

    #43 - Prepared project for inclusion into Spring Data release train.
    Moved to Spring Data parent build.

--- src/test/java/org/springframework/data/envers/repository/support/RepositoryIntegrationTests.java
+++ src/test/java/org/springframework/data/envers/repository/support/RepositoryIntegrationTests.java
@@ -46,7 +46,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = Config.class)
-public class RepositoryIntegrationTest {
+public class RepositoryIntegrationTests {
 
 	@Autowired LicenseRepository licenseRepository;
 	@Autowired CountryRepository countryRepository;
