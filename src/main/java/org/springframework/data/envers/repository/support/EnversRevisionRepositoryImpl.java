c6c4f0c6bf190cb0c23986d29c2a1100e8150d50
Julien Millau <mxjulien@gmail.com>
2018-11-13 01:35:13 +0100

    #146 - Avoid executing IN-query with empty parameter list.
    These queries are illegal in Postgres and possible other databases.
    
    Original pull request: #147.

--- src/main/java/org/springframework/data/envers/repository/support/EnversRevisionRepositoryImpl.java
+++ src/main/java/org/springframework/data/envers/repository/support/EnversRevisionRepositoryImpl.java
@@ -52,6 +52,7 @@ import org.springframework.util.Assert;
  * @author Philipp Huegelmeyer
  * @author Michael Igler
  * @author Jens Schauder
+ * @author Julien Millau
  */
 @Transactional(readOnly = true)
 public class EnversRevisionRepositoryImpl<T, ID, N extends Number & Comparable<N>>
@@ -150,8 +151,8 @@ public class EnversRevisionRepositoryImpl<T, ID, N extends Number & Comparable<N
 			Collections.reverse(revisionNumbers);
 		}
 
-		if (pageable.getOffset() > revisionNumbers.size()) {
-			return new PageImpl<Revision<N, T>>(Collections.<Revision<N, T>> emptyList(), pageable, 0);
+		if (revisionNumbers.isEmpty() || pageable.getOffset() > revisionNumbers.size()) {
+			return Page.empty(pageable);
 		}
 
 		long upperBound = pageable.getOffset() + pageable.getPageSize();
