9f3147053ce200eb52e556ee37ae9c01a1a810d1
Mark Paluch <mpaluch@vmware.com>
2021-01-12 11:24:26 +0100

    Update copyright year to 2021.
    Closes #271.

--- src/main/java/org/springframework/data/envers/repository/support/DefaultRevisionEntityInformation.java
+++ src/main/java/org/springframework/data/envers/repository/support/DefaultRevisionEntityInformation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ import org.springframework.data.repository.history.support.RevisionEntityInforma
 
 /**
  * {@link RevisionEntityInformation} for {@link DefaultRevisionEntity}.
- * 
+ *
  * @author Oliver Gierke
  */
 class DefaultRevisionEntityInformation implements RevisionEntityInformation {
--- src/test/java/org/springframework/data/envers/sample/Country.java
+++ src/test/java/org/springframework/data/envers/sample/Country.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2020 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,7 +22,7 @@ import org.hibernate.envers.Audited;
 
 /**
  * Sample domain class.
- * 
+ *
  * @author Oliver Gierke
  * @author Jens Schauder
  */
